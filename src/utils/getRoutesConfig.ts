e70579713b501b8b9715185daf8af04288218241
WolfMAN <aria.azadi.pour@protonmail.com>
2020-11-09 10:49:06 +0330

    get routes config

---
+++ src/utils/getRoutesConfig.ts
@@ -0,0 +1,34 @@
+import {RequestHandler, RouterOptions} from 'express';
+
+export type Method = 'get' | 'post' | 'put' | 'patch' | 'delete';
+
+export interface Route {
+    method: Method;
+    stack: RequestHandler[];
+}
+
+export interface RoutesConfigClass {
+    __routes_config__?: RoutesConfig;
+}
+
+export interface RoutesConfig {
+    url: string;
+    routerOptions?: RouterOptions;
+    routes: {
+        [key: string]: Route;
+    };
+    middleware: RequestHandler[];
+    subControllers: RoutesConfigClass[];
+}
+
+export default function getRoutesConfig(target: RoutesConfigClass): RoutesConfig {
+    if (!target.__routes_config__)
+        target.__routes_config__ = {
+            url: '',
+            routes: {},
+            middleware: [],
+            subControllers: []
+        };
+
+    return target.__routes_config__;
+}
